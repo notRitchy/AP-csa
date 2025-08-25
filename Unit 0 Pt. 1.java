712f3e904f4bff2ff6b7a0a3aa783570ed9ed15e
notRitchy <130450@epsstudents.org>
2025-08-25 15:43:26 -0400

    Add files via upload

--- Unit 0 Pt. 1.java
+++ Unit 0 Pt. 1.java
@@ -1,10 +1,18 @@
-class Main{
-    public static void main(String[] args){
-       System.out.print("RRRRRR       SSSSSSS\n");
-       System.out.print("RR   RR     SS\n");
-       System.out.print("RR   RR     SS\n");
-       System.out.print("RR RR        SSSSSS\n");
-       System.out.print("RR  RR             SS\n");
-       System.out.print("RR    RR    SSSSSSSS\n");
+/*
+ * Name: Ritchy Samedy
+ * Date: 08/25/2025
+ * Title:AP CSA Project 0
+ */
+
+class Main {
+    public static void main(String[] args) {
+        // Display  letters "R" and "S" using text characters
+        
+        System.out.print("RRRRRR       SSSSSSS\n");    // Top of both letters
+        System.out.print("RR   RR     SS\n");          // Upper middle section
+        System.out.print("RR   RR     SS\n");          // Upper middle section (repeated)
+        System.out.print("RR RR        SSSSSS\n");     // Middle section with connection/curve
+        System.out.print("RR  RR             SS\n");   // Lower middle section
+        System.out.print("RR    RR    SSSSSSSS\n");    // Bottom of both letters
     }
 }
