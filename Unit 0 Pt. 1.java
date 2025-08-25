ca2d046912f6cf302ad8e38fa1b51143be15483d
notRitchy <130450@epsstudents.org>
2025-08-25 15:39:36 -0400

    Add files via upload

---
+++ Unit 0 Pt. 1.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+       System.out.print("RRRRRR       SSSSSSS\n");
+       System.out.print("RR   RR     SS\n");
+       System.out.print("RR   RR     SS\n");
+       System.out.print("RR RR        SSSSSS\n");
+       System.out.print("RR  RR             SS\n");
+       System.out.print("RR    RR    SSSSSSSS\n");
+    }
+}
